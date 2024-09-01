639a0b63d79aa343363fec5395a537aae880957b
davi <davi.araujo@onfly.com.br>
2024-09-01 16:21:36 -0300

    add user entity

--- src/user/domain/entities/user.entity.ts
+++ src/user/domain/entities/user.entity.ts
@@ -1,3 +1,14 @@
+export type UserProps = {
+  name: string,
+  email: string,
+  password: string,
+  createdAt?: Date
+}
+
 export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+
 
 }
