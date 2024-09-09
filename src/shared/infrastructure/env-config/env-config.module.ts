ec3569bc6cf6427a4f60df3d15efe529362be613
davi <davi.araujo@onfly.com.br>
2024-09-09 20:44:13 -0300

    env config

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,9 +1,14 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import * as process from 'node:process';
-import { join } from 'node:path'
+import { join } from 'node:path';
+import { EnvConfigService } from '@/shared/infrastructure/env-config/env-config.service';
 
-@Module({})
+@Module({
+  imports: [ConfigModule],
+  providers: [EnvConfigService],
+  exports: [EnvConfigService],
+})
 export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
     return super.forRoot({
