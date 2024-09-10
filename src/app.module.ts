9ab78838d6111e859dab0168a68ee2ab462c0e18
davi <davi.araujo@onfly.com.br>
2024-09-10 20:19:36 -0300

    update env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UserModule } from './user/infrastructure/user.module';
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
-  imports: [EnvConfigModule, UserModule],
+  imports: [EnvConfigModule, UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
