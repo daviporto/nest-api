c7ee2da08aa15847c922d122060331a16efd8950
davi <davi.araujo@onfly.com.br>
2024-09-12 12:28:43 -0300

    implement model mapper tests

--- src/user/infrastructure/database/prisma/models/__tests__/integration/user-model.mapper.int-spec.ts
+++ src/user/infrastructure/database/prisma/models/__tests__/integration/user-model.mapper.int-spec.ts
@@ -1,17 +1,20 @@
 import { PrismaService } from '@/shared/infrastructure/database/prisma/prisma.service';
-import { execSync } from 'node:child_process';
 import { User } from '@prisma/client';
 import { UserModelMapper } from '@/user/infrastructure/database/prisma/models/user-model.mapper';
 import { ValidationErrors } from '@/shared/domain/errors/validation-errors';
+import { UserEntity } from '@/user/domain/entities/user.entity';
+import { UserDataBuilder } from '@/user/domain/testing/helper/user-data-builder';
+import { setUpPrismaTest } from '@/shared/infrastructure/database/prisma/testing/set-up-prisma-test';
 
 describe('User model mapper integration tests', () => {
   let prismaService: PrismaService;
   let props: any;
 
   beforeAll(async () => {
-    execSync('npm run prisma:migrate-test');
-
+    setUpPrismaTest();
+    
     prismaService = new PrismaService();
+    props = UserDataBuilder({});
     await prismaService.$connect();
   });
 
@@ -29,5 +32,21 @@ describe('User model mapper integration tests', () => {
     expect(() => UserModelMapper.toEntity(model)).toThrowError(
       new ValidationErrors('Could not load user having id undefined'),
     );
+  });
+
+  it('should map user model to entity', async () => {
+    const model: User = await prismaService.user.create({
+      data: props,
+    });
+
+    const sut = UserModelMapper.toEntity(model);
+
+    expect(sut).toBeInstanceOf(UserEntity);
+
+    expect(sut.id).toBe(model.id);
+    expect(sut.name).toBe(model.name);
+    expect(sut.email).toBe(model.email);
+    expect(sut.password).toBe(model.password);
+    expect(sut.createdAt).toBe(model.createdAt);
   });
 });
