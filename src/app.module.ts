767dfef5c6c46c0b1a0875637816e3fd629b3643
davi <davi.araujo@onfly.com.br>
2024-09-14 21:52:41 -0300

    initialize auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UserModule } from './user/infrastructure/user.module';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
+import { AuthModule } from './auth/infrastructure/auth.module';
 
 @Module({
-  imports: [EnvConfigModule, UserModule, DatabaseModule],
+  imports: [EnvConfigModule, UserModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [],
 })
