7c6947264f967c054c41b1356d11f52fd74897ae
davi <davi.araujo@onfly.com.br>
2024-09-16 16:59:00 -0300

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,35 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
 import { applyGlobalConfig } from '@/global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Auth Api')
+    .setDescription(
+      'The Nest Api support creating deleting editing and authenticating users',
+    )
+    .setVersion('1.0')
+    .addBearerAuth({
+      name: 'Authorization',
+      description: 'JWT token',
+      in: 'header',
+      type: 'http',
+      scheme: 'bearer',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   applyGlobalConfig(app);
   await app.listen(3000, '0.0.0.0');
